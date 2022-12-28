ea04ccd492d56723001201f01c8dfd15d112f4fc
SawyerRoP <netsmut59@gmail.com>
2022-12-29 06:37:40 +0700

    add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,47 +15,43 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        try{
+        try {
             Jsoup.connect("http://119.160.219.22/hlpdsk_pcc/index.php").timeout(6000).get();
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.exit(0);
         }
 
         DateTimeFormatter date = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
         String dateStart = now.toString();
-        String year = dateStart.substring(0,4);
-        String mount = dateStart.substring(5,7);
-        String day = dateStart.substring(8,10);
+        String year = dateStart.substring(0, 4);
+        String mount = dateStart.substring(5, 7);
+        String day = dateStart.substring(8, 10);
         Calendar cal = Calendar.getInstance();
         int dayOfMount = cal.getLeastMaximum(Calendar.DAY_OF_MONTH);
         LocalDate today = LocalDate.now();
         LocalDate lastOfMount = today.withDayOfMonth(today.lengthOfMonth());
         String d = lastOfMount.toString();
-        String lastDate = d.substring(8,10);
-        String lastMount = d.substring(5,7);
+        String lastDate = d.substring(8, 10);
+        String lastMount = d.substring(5, 7);
         Document getCookie = Jsoup.connect("http://119.160.219.22/hlpdsk_pcc/index.php").timeout(6000).get();
         String cookie = getCookie.connection().cookieStore().getCookies().get(0).getValue();
-        Document data = Jsoup.connect("http://119.160.219.22/hlpdsk_pcc/check_login.php").data("username","tech").data("password","lhs2541").data("Submit2","").cookie("PHPSESSID",cookie).timeout(6000).post();
-        Document getCall = data.connection().url("http://119.160.219.22/hlpdsk_pcc/helpdesk/report_by_supplier2.php").data("id_project","ALL").
-                data("type_service","ALL").data("date_start", "26" + "-" + mount + "-" + year).data("date_end",lastDate + "-" + lastMount+ "-" + year).data("start","0").
-                data("submit","Find").data("User-Agent"," Mozilla/5.0 (Macintosh").cookie("PHPSESSID",cookie).timeout(6000).post();
-
-
+        Document data = Jsoup.connect("http://119.160.219.22/hlpdsk_pcc/check_login.php").data("username", "tech").data("password", "lhs2541").data("Submit2", "").cookie("PHPSESSID", cookie).timeout(6000).post();
+        Document getCall = data.connection().url("http://119.160.219.22/hlpdsk_pcc/helpdesk/report_by_supplier2.php").data("id_project", "ALL").
+                data("type_service", "ALL").data("date_start", "27" + "-" + mount + "-" + year).data("date_end", lastDate + "-" + lastMount + "-" + year).data("start", "0").
+                data("submit", "Find").data("User-Agent", " Mozilla/5.0 (Macintosh").cookie("PHPSESSID", cookie).timeout(6000).post();
 
 
         //Get total call from 10.20.15.38
-        int post = getCall.getElementsByTag("tr").size()-1;
+        int post = getCall.getElementsByTag("tr").size() - 1;
         String location = args[0];
-        System.out.println(location);
+        //System.out.println(location);
 
         // Write calls to file
 
 
-
-
         System.out.println(post);
-        System.out.println(day);
+        //System.out.println(day);
         //System.out.println(getCall.body().getElementsByTag("tr").get(2).text());
         //String sendData = getCall.body().getElementsByTag("tr").get(2).text();
         //System.out.println(send);
@@ -85,47 +81,47 @@ public class Main {
         // System.out.println(checkDate);
 
 
-
-
         //get count call from Google sheets
         Document countCall = Jsoup.connect("https://script.google.com/macros/s/AKfycbyA4aMWncFv-rD2zscnty1Qh6WL4qK2GwflKrudP4TtxgUxyT1M0srNBCIVc2dQFF2YOw/exec?action=getCall").ignoreContentType(true).get();
-        //System.out.println(countCall.text());
+        // System.out.println(countCall.text());
 
         //regex data from Google sheet
         Pattern pattern = Pattern.compile("\\d");
-        Matcher matcher =  pattern.matcher(countCall.text());
+        Matcher matcher = pattern.matcher(countCall.text());
         String call = "";
         while (matcher.find()) {
             call += matcher.group();
 
         }
-        System.out.println(call);
-        int oldCall = Integer.parseInt(call.substring(0,call.length() - 1));
+        //System.out.println(call);
+        int oldCall = Integer.parseInt(call.substring(0, call.length() - 1));
         String lastCall = Integer.toString(post);
 
         //Check call update
-        String send = call.substring(0,call.length() -1);
-        System.out.println(send);
-        if(post > oldCall) {
+        String send = call.substring(0, call.length() - 1);
+        //System.out.println(send);
+        if (post > oldCall) {
             String compare = Integer.toString(post - (post - oldCall));
             System.out.println(compare);
-            Document getCallCompare = data.connection().url("http://119.160.219.22/hlpdsk_pcc/helpdesk/report_by_supplier2.php").data("id_project","ALL").
-                    data("type_service","ALL").data("date_start", "26" + "-" + mount + "-" + year).data("date_end",lastDate + "-" + lastMount+ "-" + year).data("start",compare).
-                    data("submit","Find").data("User-Agent"," Mozilla/5.0 (Macintosh").cookie("PHPSESSID",cookie).timeout(6000).post();
+            Document getCallCompare = data.connection().url("http://119.160.219.22/hlpdsk_pcc/helpdesk/report_by_supplier2.php").data("id_project", "ALL").
+                    data("type_service", "ALL").data("date_start", "27" + "-" + mount + "-" + year).data("date_end", lastDate + "-" + lastMount + "-" + year).data("start", compare).
+                    data("submit", "Find").data("User-Agent", " Mozilla/5.0 (Macintosh").cookie("PHPSESSID", cookie).timeout(6000).post();
 
             //System.out.println(a);
-            System.out.println(post - oldCall);
-
+            //System.out.println(post - oldCall);
+            Jsoup.connect("https://bot-production-8999.up.railway.app/post").data(" ",location).data("location",location).ignoreContentType(true).post();
 
             for(int i=0;i < (post-oldCall);i++) {
-                String sendData = getCallCompare.body().getElementsByTag("tr").get(i+1).text();
-                Jsoup.connect("https://rdcall-production.up.railway.app/post").data(" ",sendData).data("locaton",location).ignoreContentType(true).post();
-                Thread.sleep(3000);
+            String sendData = getCallCompare.body().getElementsByTag("tr").get(i+1).text();
+            Jsoup.connect("https://bot-production-8999.up.railway.app/post").data(" ", sendData).data("location", location).ignoreContentType(true).post();
+            Thread.sleep(3000);
             }
+            String a = Integer.toString(post);
+            Jsoup.connect("https://script.google.com/macros/s/AKfycbyA4aMWncFv-rD2zscnty1Qh6WL4qK2GwflKrudP4TtxgUxyT1M0srNBCIVc2dQFF2YOw/exec?").data("action","update").data("name",a).ignoreContentType(true).get();
 
-        }else {
-            System.exit(0);
-        }
+            }else {
+             System.exit(0);
+            }
     }
 
 }
